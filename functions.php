f1a35311b4e6ade66cd67e73bfc72000ef748b1f
momotan <momotaha039@gmail.com>
2024-02-21 22:02:53 +0200

    replace style with rtl style to be fit for arabic

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 function includeStyleFiles(){
     wp_enqueue_style('style', get_stylesheet_uri());
+    // make it fit for arabic
+    wp_style_add_data('style','rtl','replace');
 }
 add_action('wp_enqueue_scripts', 'includeStyleFiles');
 ?>
